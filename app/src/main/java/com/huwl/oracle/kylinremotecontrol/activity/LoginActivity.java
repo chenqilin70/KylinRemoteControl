26d77c4e629f2930ab44c819153dd2b36927e4cc
KylinChen <kylinchen85@foxmail.com>
2017-09-16 22:23:23 +0800

    登录界面

--- app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/LoginActivity.java
+++ app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/LoginActivity.java
@@ -1,15 +1,52 @@
 package com.huwl.oracle.kylinremotecontrol.activity;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.huwl.oracle.kylinremotecontrol.R;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends Activity implements TextWatcher {
+    private EditText username_et,password_et;
+    private ImageView loginBtn;
+    private TextView login_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username_et=findViewById(R.id.username_et);
+        password_et=findViewById(R.id.password_et);
+        loginBtn=findViewById(R.id.loginBtn);
+        login_text=findViewById(R.id.login_text);
+        username_et.addTextChangedListener(this);
+        password_et.addTextChangedListener(this);
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if((!"".equals(username_et.getText().toString())) && (!"".equals(password_et.getText().toString()))){
+            loginBtn.setImageResource(R.drawable.login_btn_active);
+            login_text.setTextColor(Color.rgb(255,255,255));
+        }else{
+            loginBtn.setImageResource(R.drawable.login_btn_unable);
+            login_text.setTextColor(Color.rgb(167,168,169));
+        }
     }
 }
--- app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/MainActivity.java
+++ app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.huwl.oracle.kylinremotecontrol.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -17,8 +18,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         animation=AnimationUtils.loadAnimation(this,R.anim.anim);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
         text_icon=findViewById(R.id.text_icon);
         text_icon.setAnimation(animation);
+
 
     }
 }
