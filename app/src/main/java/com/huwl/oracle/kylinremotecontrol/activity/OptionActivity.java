3bab3e5ae0ae4e30e0b91e32bf3f0b1b62d3363a
KylinChen <kylinchen85@foxmail.com>
2017-09-25 17:44:25 +0800

    势力扩大解放山东

--- app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/OptionActivity.java
+++ app/src/main/java/com/huwl/oracle/kylinremotecontrol/activity/OptionActivity.java
@@ -1,17 +1,28 @@
 package com.huwl.oracle.kylinremotecontrol.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.dtr.zxing.activity.CaptureActivity;
 import com.huwl.oracle.kylinremotecontrol.R;
 import com.huwl.oracle.kylinremotecontrol.util.BarUtils;
 
-public class OptionActivity extends Activity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OptionActivity extends Activity implements View.OnClickListener {
+    private ImageView scanIcon;
+    private ListView terminalsLV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +33,25 @@ public class OptionActivity extends Activity {
             getWindow().addFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
+        scanIcon=findViewById(R.id.scanIcon);
+        terminalsLV=findViewById(R.id.terminalsLV);
+        scanIcon.setOnClickListener(this);
+        List<Map<String,String>> listItem=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Map<String,String> item=new HashMap();
+            item.put("terminalNameTV","aierxuan"+i);
+            item.put("terminalTypeTV","Windows 10"+"  "+i);
+            listItem.add(item);
+        }
+        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,//需要绑定的数据
+                R.layout.terminal_item,//每一行的布局
+                //动态数组中的数据源的键对应到定义布局的View中
+                new String[] {"terminalNameTV","terminalTypeTV"},
+                new int[] {R.id.terminalNameTV,R.id.terminalTypeTV}
+            );
+        terminalsLV.setAdapter(mSimpleAdapter);
+
     }
-
-
-
 
     @Override
     protected void onDestroy() {
@@ -35,4 +61,10 @@ public class OptionActivity extends Activity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        if(view==scanIcon){
+            startActivity(new Intent(this,CaptureActivity.class));
+        }
+    }
 }
