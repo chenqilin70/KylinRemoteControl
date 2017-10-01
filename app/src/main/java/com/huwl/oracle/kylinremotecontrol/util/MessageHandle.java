c82a0d6e97be25f0f5c2065b77860ae80831744e
KylinChen <kylinchen85@foxmail.com>
2017-10-01 23:42:07 +0800

    sdfdsui

--- app/src/main/java/com/huwl/oracle/kylinremotecontrol/util/MessageHandle.java
+++ app/src/main/java/com/huwl/oracle/kylinremotecontrol/util/MessageHandle.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.huwl.oracle.kylinremotecontrol.R;
 import com.huwl.oracle.kylinremotecontrol.activity.LoginActivity;
 import com.huwl.oracle.kylinremotecontrol.activity.MainActivity;
 import com.huwl.oracle.kylinremotecontrol.activity.OptionActivity;
@@ -26,6 +30,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by aierxuan on 2017/9/18.
@@ -85,14 +91,51 @@ public class MessageHandle {
         }.start();
     }
     public static void handleAddControllableTer(final NetMessage m){
-        Terminal t= (Terminal) m.getMap().get("terminal");
-        OptionActivity oa= (OptionActivity) activities.get(OptionActivity.class);
-        Set<Terminal> ts= (Set<Terminal>) m.getMap().get("otherTerminal");
-        List<Map<String,Object>> list=oa.getListItem();
-        for(Terminal tempTer:ts){
+        final OptionActivity oa= (OptionActivity) activities.get(OptionActivity.class);
+        Toast.makeText(oa,"收到刷新listview的消息了",Toast.LENGTH_SHORT).show();
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+
+                Set<Terminal> ts= (Set<Terminal>) m.getMap().get("otherTerminal");
+                Toast.makeText(oa,GSON.toJson(ts),Toast.LENGTH_SHORT).show();
+                try{
+                    List<Map<String,Object>> list=oa.getListItem();
+                    list.clear();
+                    for(Terminal tempTer:ts){
+                        if(tempTer.getSystemType().toLowerCase().contains("window")){
+                            Map<String,Object> item=new HashMap();
+                            item.put("terminalNameTV",tempTer.getName());
+                            item.put("terminalTypeTV",tempTer.getSystemType());
+                            item.put("radioImg", R.drawable.radio_unselect);
+                            item.put("selected",false);
+                            item.put("id",tempTer.getId());
+                            list.add(item);
+                        }
+                    }
+                    final ListView lv=oa.getTerminalsLV();
+                    oa.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((SimpleAdapter)lv.getAdapter()).notifyDataSetChanged();
+                            oa.findViewById(R.id.noTerminalRL).setVisibility(View.GONE);
+                            oa.findViewById(R.id.terminalsLV).setVisibility(View.VISIBLE);
+                        }
+                    });
+                }catch (final Exception e){
+                    NetMessage em=new NetMessage();
+                    em.getMap().put("e",e);
+                    em.setForWhat(NetMessage.EXCEPTION);
+                    em.send(server);
+                }
+            }
+        };
+        timer.schedule(task,3000);
 
 
-        }
+
+
     }
     public static void handleAutoLogin(final NetMessage m){
         MainActivity ma= (MainActivity) activities.get(MainActivity.class);
